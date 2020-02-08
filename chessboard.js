2d628461a7ebddef49b8fae94a1cf41efc8445c4
matthingly-git <matthew.cesar96@gmail.com>
2020-02-08 16:43:30 +0100

    100% funzionante

--- chessboard.js
+++ chessboard.js
@@ -3,3 +3,32 @@
 * At each position of the grid there is either a space or a "#" character. 
 * The characters should form a chessboard.
 */
+
+let board = "";
+let size = 8;
+for (var j = 0; j < size; j++) {
+
+  for (var i = 0; i < size; i++) {
+
+    if (j % 2 === 0) {
+      if (i === size - 1) {
+        board += "\n";
+      } else if (board.length % 2 === 0) {
+        board += "#";
+      } else {
+        board += " ";
+      }
+    } else {
+      if (i === size - 1) {
+        board += "\n";
+      } else if (board.length % 2 === 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+
+  }
+
+}
+console.log(board);
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,3 +4,36 @@
 * When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 * (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
+
+//FizzBuzz Parte 1
+/*
+let a = 1;
+
+for (let i = 0; i < 100; i++) {
+  if (a % 3 === 0) {
+    console.log("Fizz");
+  } else if (a % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(a);
+  }
+  a++;
+}
+*/
+
+
+//FizzBuzz Parte 2
+let a = 1;
+
+for (let i = 0; i < 100; i++) {
+  if (a % 5 === 0 && a % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (a % 3 === 0) {
+    console.log("Fizz");
+  } else if (a % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(a);
+  }
+  a++;
+}
